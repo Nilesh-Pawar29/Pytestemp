2b9059c80d6b7a61265ee2f6b4df9844bcbaf60b
Nilesh29017 <nileshpawar2901@gmail.com>
2025-12-02 09:03:09 +0530

    Emp

--- employee.py
+++ employee.py
@@ -1,9 +1,9 @@
 import pytest
 def employee_details(name, emp_id, department, salary):
     result = (
-        f"Employee Name: {name}\n",
-        f"Employee ID: {emp_id}\n",
-        f"Department: {department}\n",
+        f"Employee Name: {name}\n"
+        f"Employee ID: {emp_id}\n"
+        f"Department: {department}\n"
         f"Salary: {salary}"
     )
     return result
